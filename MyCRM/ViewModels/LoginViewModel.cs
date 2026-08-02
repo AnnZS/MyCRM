fc0cbe92b96bf9d70d6b5c1d5f6473a4c780d610
Anna Stanisławska <anna.stanislawska9845@gmail.com>
2026-08-02 22:45:44 +0200

    Dodanie klasy LoginViewModel i określenie jej właściwości

---
+++ MyCRM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCRM.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; } = false;
+    }
+}
